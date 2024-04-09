ae797066f514a286b6e9e8dc692b3fa49ab8c4d5
Archilece <johnrigelqgaleos@gmail.com>
2024-04-10 00:55:50 +0800

    routing

--- Hi-B/src/app/app-routing.module.ts
+++ Hi-B/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { DoctorHomePageComponent } from './doctor-home-page/doctor-home-page.com
 import { RegistrationNurseAddDetailsComponent } from './registration-nurse-add-details/registration-nurse-add-details.component';
 import { RealDoctorHomepageComponent } from './real-doctor-homepage/real-doctor-homepage.component';
 import { RegistrationNursePatientPageComponent } from './registration-nurse-patient-page/registration-nurse-patient-page.component';
+import { AddDailyReportComponent } from './add-daily-report/add-daily-report.component';
 const routes: Routes = [
   {path: 'nurse-page', component: NurseHomePageComponent},
   {path: 'report-page', component: DoctorHomePageComponent},
   {path: 'doctor-add', component: RegistrationNurseAddDetailsComponent},
   {path: 'doctor-page', component:RealDoctorHomepageComponent},
-  {path: 'nurse-daily', component:RegistrationNursePatientPageComponent}
+  {path: 'nurse-daily', component:AddDailyReportComponent}
 ];
 
 @NgModule({
